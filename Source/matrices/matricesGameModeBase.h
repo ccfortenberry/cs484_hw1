fdd561ea86eaf6e2cdeb62755383527320462da1
ccfortenberry <cmnky@hotmail.com>
2019-09-09 00:10:46 -0800

    add hw without large files preventing push due to large file size

---
+++ Source/matrices/matricesGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "matricesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATRICES_API AmatricesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
